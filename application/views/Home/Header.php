de6afd2959792cfc42faaf1eb121b78c9837d271
KP <korntip_ok@hotmail.com>
2017-02-02 14:16:40 +0700

    Commit!!

---
+++ application/views/Home/Header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!--<meta http-equiv="Content-Type" content="text/html; charset=windows-874" />-->
+<!-- Required meta tags -->
+    <meta charset="windows-874">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+	<title>iProjectOrder</title>
+
+<!-- CSS -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<!-- END CSS -->
+
+</head>
+
+<body>
+<!-- Body -->
+	<h3>Hello</h3>
+    <h4>สวัสดี</h4>
+<!-- / Body -->
+
+<!-- SCRIPT -->
+	<script src="<?php echo base_url('assets/js/jquery-3.1.1.slim.min.js') ;?>" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url('assets/js/tether.min.js'); ?>" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ;?>" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<!-- END SCRIPT -->
+
+</body>
+</html>
